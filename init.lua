02931c648b2b2b2c717e9b09ca4fddd543df668c
Carter <carter@cartercall.com>
2025-06-19 23:01:44 -0400

    add markdown lsp

--- init.lua
+++ init.lua
@@ -588,6 +588,7 @@ require('lazy').setup({
         -- clangd = {},
         gopls = {},
         pyright = {},
+        marksman = {},
 
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
